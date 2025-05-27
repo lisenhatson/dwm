930ad59483a91804db0f910966c872bffdf87f74
Lisen Hatson <alimanthereploid@gmail.com>
2025-05-27 13:06:07 +0700

    use monospace

--- config.h
+++ config.h
@@ -26,7 +26,7 @@ static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 0;       /* vertical padding of bar */
 static const int sidepad            = 0;       /* horizontal padding of bar */
-static char *fonts[]          = { "uzura_font:size=12", "monospace:size=12", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"  };
+static char *fonts[]          = { "monospace:size=12", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
